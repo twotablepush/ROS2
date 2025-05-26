504060ac3779bdb743ac94528b885d7f770586ff
twotablepush <ssangmin7252@gmail.com>
2025-05-26 18:23:23 +0900

    add : Subscriber_Node

--- tutorial_topic/setup.py
+++ tutorial_topic/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'move_publisher = tutorial_topic.move_publisher:main',
+            'lidar_subscriber = tutorial_topic.lidar_subscriber:main',
         ],
     },
 )
