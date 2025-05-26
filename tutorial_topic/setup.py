c2e7de8e7ae6bc5a812d276ffaf3c2c70ee1cdfa
twotablepush <ssangmin7252@gmail.com>
2025-05-26 17:41:37 +0900

    add : Publisher

---
+++ tutorial_topic/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tutorial_topic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tablepush',
+    maintainer_email='tablepush@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_publisher = tutorial_topic.move_publisher:main',
+        ],
+    },
+)
